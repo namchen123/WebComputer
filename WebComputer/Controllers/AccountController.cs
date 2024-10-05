2fa9c035c96df370df19ee8578423d494ef5a0fa
namchen123 <163426959+namchen123@users.noreply.github.com>
2024-10-05 19:17:36 +0700

    new

--- WebComputer/Controllers/AccountController.cs
+++ WebComputer/Controllers/AccountController.cs
@@ -25,6 +25,7 @@ namespace WebComputer.Controllers
             if (account == null || account.PasswordHash != model.Password)
             {
                 ModelState.AddModelError(string.Empty, "Invalid username or password");
+                TempData["Message"] = "Invalid username or password";
                 return View(model);
             }
             var claims = new List<Claim>
@@ -53,6 +54,55 @@ namespace WebComputer.Controllers
         {
             HttpContext.SignOutAsync();
             return RedirectToAction("Index", "HomePage");
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid) {
+                TempData["Message"] = "Please re-enter";
+                return View();
+            }
+            if(model.Password != model.Password2)
+            {
+                TempData["Message"] = "Incorrect re-enter password";
+                return View();
+            }
+            var account = _storeContext.Accounts.Where(p=>p.Email==model.Username);
+            if (account.Any()==true)
+            {
+                TempData["Message"] = "Username was used";
+                return View();
+            }
+            Account newAccount = new Account();
+            newAccount.Email = model.Username;
+            newAccount.PasswordHash = model.Password;
+            newAccount.CreatedDate = DateTime.UtcNow;
+            newAccount.Role = "KH";
+            _storeContext.Accounts.AddAsync(newAccount);
+            _storeContext.SaveChangesAsync();
+            TempData["Message"] = "Create account success";
+            return RedirectToAction("Login","Account");
+        }
+
+        public IActionResult CustomerDetail()
+        {
+            var customer = _storeContext.Customers.SingleOrDefault(p=>p.Account.Email.Equals(User.Identity.Name));
+            return View(customer);
+        }
+        [HttpPost]
+        public IActionResult CustomerDetail(Customer customer)
+        {
+            
+                customer.AccountId = _storeContext.Accounts.SingleOrDefault(p => p.Email.Equals(User.Identity.Name)).AccountId;
+                _storeContext.Customers.Add(customer);
+                _storeContext.SaveChanges();
+            return View(customer);
         }
     }
     
---
+++ WebComputer/Models/RegisterViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebComputer.Models
+{
+    public class RegisterViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Password2 { get; set; }
+
+    }
+}
