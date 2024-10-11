99e53a1f27ab1d29664e237560fa1e2c6277fb05
namchen123 <163426959+namchen123@users.noreply.github.com>
2024-10-11 13:58:55 +0700

    a

--- WebComputer/Controllers/HomePageController.cs
+++ WebComputer/Controllers/HomePageController.cs
@@ -20,10 +20,15 @@ namespace WebComputer.Controllers
                 ViewBag.cartitemcount = _storeContext.Customers.Where(x => x.AccountId == accountid).Include(p => p.Carts).ThenInclude(a => a.CartItems).SelectMany(c=>c.Carts).SelectMany(d=>d.CartItems).Count();
             }
             var laptop = _storeContext.Products.Where(p=>p.CategoryId==1).Take(5);
-            var console = _storeContext.Products.Where(p => p.CategoryId == 17 || p.CategoryId == 16).Take(5);
+            var console = _storeContext.Products.Where(p => p.CategoryId == 17 || p.CategoryId == 16).OrderBy(p => Guid.NewGuid()).Take(5);
+            var keyboard = _storeContext.Products.Where(p=> p.CategoryId == 13).Take(5);
+            var mouse = _storeContext.Products.Where(p=>p.CategoryId == 12).Take(5);
+
             ViewBag.laptop = laptop;
             ViewBag.console = console;
-            
+            ViewBag.keyboard = keyboard;
+            ViewBag.mouse = mouse;
+
             return View(laptop);
         }
     }
